97066606b104a8d9c53cea9e73c715fb6feac60d
khyatika <khyatika.sawani025@gmail.com>
2025-06-25 15:49:04 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,16 +1,14 @@
-erraform {
+terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.26.0"
     }
   }
+
+  backend "azurerm" {}
 }
 provider "azurerm" {
   features {}
-  subscription_id = "7b5bc41a-d156-450e-bf00-1cfe69176584"
-}
-
-terraform {
-  backend "azurerm" {}
+  subscription_id = "76f3c62f-e8ff-4cba-8eb2-11e11a9c35e5"
 }
